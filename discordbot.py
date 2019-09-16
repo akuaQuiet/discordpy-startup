f366cc89821eb48a2dc06aab52f8b9ed5b61fae5
akuaQuiet <55394741+akuaQuiet@users.noreply.github.com>
2019-09-17 01:22:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -21,58 +21,6 @@ async def うんち(ctx):
     await ctx.send(':poop:')
     
 
-    import discord
-from discord.ext import commands
-import os
 
-bot = commands.Bot(command_prefix='!')
-
-
-@bot.group(
-    pass_context=True
-)
-async def role(ctx: commands.Context):
-    """
-    役職を管理するコマンド
-    例：!role set テスト
-    """
-    if ctx.invoked_subcommand is None:
-        await bot.say("サブコマンドset, unsetのどちらかを呼んでください。")
-
-
-@role.command(
-    name="set",
-    pass_context=True
-)
-async def set_role(ctx: commands.Context, *roles: discord.Role):
-    """
-    役職を得るコマンド
-    例：!role set テスト
-    """
-    if not roles:
-        await bot.say("不正な入力です。")
-        return
-
-    user = ctx.message.author
-    await bot.add_roles(user, *roles)
-    await bot.say("役職を追加しました。")
-
-
-@role.command(
-    name="unset",
-    pass_context=True
-)
-async def unset_role(ctx: commands.Context, *roles: discord.Role):
-    """
-    役職を消すコマンド
-    例：!role unset テスト
-    """
-    if not roles:
-        await bot.say("不正な入力です。")
-        return
-
-    user = ctx.message.author
-    await bot.remove_roles(user, *roles)
-    await bot.say("役職を解除しました。")
 
 bot.run(token)

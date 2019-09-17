7b4e6960995f68a323cebdf47f54fe1465b483c9
akuaQuiet <55394741+akuaQuiet@users.noreply.github.com>
2019-09-17 22:05:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -20,6 +20,13 @@ async def ping(ctx):
 async def うんち(ctx):
     await ctx.send(':poop:')
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('/join'):
+        role = discord.utils.get(message.guild.roles, name='Test')
+        await message.author.add_roles(role)
+        reply = f'{message.author.mention} ようこそ！'
+        await message.channel.send(reply)
 
 bot.run(token)
 

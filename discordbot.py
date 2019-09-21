243b0eb88080d12a213170bea8439897f445ef48
akuaQuiet <55394741+akuaQuiet@users.noreply.github.com>
2019-09-21 15:05:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,40 +1,19 @@
-import discord
+from discord.ext import commands
+import os
+import traceback
 
-# 自分のBotのアクセストークンに置き換えてください
-TOKEN = 'NjIzMTQ4ODM5MTA4NjA4MDI4.XYEiRA.0ePkZgdKdkfz0tDT70j1AUJDG7A'
-
-# 接続に必要なオブジェクトを生成
-client = discord.Client()
-
-# 起動時に動作する処理
-@client.event
-async def on_ready():
-    # 起動したらターミナルにログイン通知が表示される
-    print('ログインしました')
+bot = commands.Bot(command_prefix='/')
+token = os.environ['DISCORD_BOT_TOKEN']
 
 
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(str(error))
 
 
-# メッセージ受信時に動作する処理
-@client.event
-async def on_message(message):
-    # メッセージ送信者がBotだった場合は無視する
-    if message.author.bot:
-        return
-    # 「/neko」と発言したら「にゃーん」が返る処理
-    if message.content == '/neko':
-        await message.channel.send('くーちゃんきゃわわ')
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
 
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('/50020'):
-        role = discord.utils.get(message.guild.roles, name='全問正解者')
-        await message.author.add_roles(role)
-        reply = f'{message.author.mention} 全問正解です！役職を付与します。'
-        await message.channel.send(reply)
-
-
-# Botの起動とDiscordサーバーへの接続
-client.run(TOKEN)
-
+bot.run(token)

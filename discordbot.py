ab2c4a583e05669b6fae38c53c4fa85724d84534
akuaQuiet <55394741+akuaQuiet@users.noreply.github.com>
2019-09-21 15:07:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,4 +16,14 @@ async def ping(ctx):
     await ctx.send('pong')
 
 
+@client.event
+async def on_message(message):
+    if message.content.startswith('/50020'):
+        role = discord.utils.get(message.guild.roles, name='全問正解者')
+        await message.author.add_roles(role)
+        reply = f'{message.author.mention} 全問正解です！役職を付与します。'
+        await message.channel.send(reply)
+    
+    
+    
 bot.run(token)

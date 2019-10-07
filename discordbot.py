5bd4715021c8b2105223279dc066b4ac3d11308c
akuaQuiet <55394741+akuaQuiet@users.noreply.github.com>
2019-10-08 07:43:12 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,6 +15,27 @@ async def on_command_error(ctx, error):
 async def akua(ctx):
     await ctx.send('ガノン窓副窓主')
 
+
+@client.event
+async def on_message(message):
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('くーちゃんきゃわわ')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('/50020'):
+        role = discord.utils.get(message.guild.roles, name='全問正解者')
+        await message.author.add_roles(role)
+        reply = f'{message.author.mention} 全問正解です！役職を付与します。'
+        await message.channel.send(reply)    
+    
+    
+    
     
     
 bot.run(token)
